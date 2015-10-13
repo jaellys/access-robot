996324f0623f9bf36243d9b6d84dc8c1e17d1cbc
jaellysbales <jaellysbales@gmail.com>
2015-10-13 00:50:23 -0400

    Fixed menu bug

--- AccessRobot/app/src/main/java/ksmaragh/c4q/nyc/accessrobot/LearnActivity.java
+++ AccessRobot/app/src/main/java/ksmaragh/c4q/nyc/accessrobot/LearnActivity.java
@@ -128,7 +128,8 @@ public class LearnActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            this.startActivity(settingsIntent);
         }
         return super.onOptionsItemSelected(item);
     }
